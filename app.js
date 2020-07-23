1bfa66fa2189e88906e6ef9bcec76812c4abd739
Marcus Young <monkeyyoung3@gmail.com>
2020-07-23 14:10:50 -0700

    changed sync room command to create adaptive card

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var util = require("util");
 var teams = require('botbuilder-teams');
 
 const { BotFrameworkAdapter, TeamsActivityHandler, CardFactory } = require('botbuilder');
+const { title } = require('process');
 
 const adapter = new BotFrameworkAdapter({
     appId: "758bb218-c046-43f1-809b-a627157f0d90",
@@ -43,115 +44,89 @@ class TeamsMessagingExtensionsActionBot extends TeamsActivityHandler {
             default: 
                 throw new Error('Not Implemented')
         }
-        
     }
 }
 
 function startMeeting(context, action){
     const data = action.data;
-        const heroCard = CardFactory.heroCard(data.title, '', [], [{
-            "type": "openUrl",
-            "title": "Start Meeting",
-            "value": "kloud.cn/kloud/documents"
-          },]);
-        heroCard.content.subtitle = data.subTitle;
-        const attachment = { contentType: heroCard.contentType, content: heroCard.content, preview: heroCard };
+    const heroCard = CardFactory.heroCard(data.title, '', [], [{
+        "type": "openUrl",
+        "title": "Start Meeting",
+        "value": "kloud.cn/kloud/documents"
+        },]);
+    heroCard.content.subtitle = data.subTitle;
+    const attachment = { contentType: heroCard.contentType, content: heroCard.content, preview: heroCard };
 
-        return {
-            composeExtension: {
-                type: 'result',
-                attachmentLayout: 'list',
-                attachments: [
-                attachment
-                ]
-            }
+    return {
+        composeExtension: {
+            type: 'result',
+            attachmentLayout: 'list',
+            attachments: [
+            attachment
+            ]
         }
+    }
 }
 
 function syncRoom(context, action){
-    const data = action.data;
-        const heroCard = CardFactory.heroCard(data.title, '', [], [{
-            "type": "openUrl",
-            "title": "Go To Sync Rooms",
-            "value": "kloud.cn/kloud/syncroom"
-          },]);
-        heroCard.content.subtitle = data.subTitle;
-        const attachment = { contentType: heroCard.contentType, content: heroCard.content, preview: heroCard };
+    const syncRoomList = [
+        { name: 'Room 1' },
+        { name: 'Room 2' },
+        { name: 'Room 3' },
+    ]
+    const choices = [];
+    syncRoomList.forEach(el => choices.push({ title: el.name, value: el.name }))
+    const adaptiveCard = CardFactory.adaptiveCard({
+        actions: [{
+          title: 'Open Sync Room',
+          type: "Action.OpenUrl",
+          url: 'https://kloud.com/'
+        }],
+        body: [
+          { text: 'Sync Rooms', type: 'TextBlock', weight: 'bolder'},
+          { choices, id: 'MultiSelect', style: 'expanded', type: 'Input.ChoiceSet' },
+        ],
+        type: 'AdaptiveCard',
+        version: '1.0'
+      });
 
-        return {
-            composeExtension: {
-                type: 'result',
-                attachmentLayout: 'list',
-                attachments: [
-                attachment
-                ]
-            }
+      return {
+        task: {
+          type: 'continue',
+          value: {
+            card: adaptiveCard,
+            height: 450,
+            title: 'Sync Room List',
+            url: null,
+            width: 500
+          }
         }
+      };
 }
 
 const bot = new TeamsMessagingExtensionsActionBot();
+const inMemoryStorage = new builder.MemoryBotStorage();
+const chatBot = new builder.UniversalBot(adapter).set('storage', inMemoryStorage);
+
+var stripBotAtMentions = new teams.StripBotAtMentions();
+chatBot.use(stripBotAtMentions);
+
+chatBot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
 
 const server = restify.createServer();
 server.listen(3978, function () {
     console.log('%s listening to %s', server.name, util.inspect(server.address()));
 });
 
-// var inMemoryStorage = new builder.MemoryBotStorage();
-// var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
-
-// this will reset and allow to receive from any tenants
-// connector.resetAllowedTenants();
-
-// var bot = new builder.UniversalBot(connector);
-
-// server.post('/api/composeExtension', connector.listen());
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         await bot.run(context);
     });
 });
-// server.post('/api/messages', connector.listen());
-// server.post('/', connector.listen());
-
-// var composeExtensionHandler = function (event, query, callback) {
-//     // parameters should be identical to manifest
-//     console.log("Query Running");
-
-//     var attachments = [];
-//         try {
-//             var card = new builder.HeroCard()
-//                 .buttons([{
-//                     type: "openUrl",
-//                     title: "Open Kloud",
-//                     value: 'kloud.com'
-//                 }]);
-
-//             attachments.push(card.toAttachment());
-
-
-//         } catch (err) {
-//             console.log(err);
-//         }
-
-
-//         var response = teams.ComposeExtensionResponse
-//             .result('list')
-//             .attachments(attachments)
-//             .toResponse();
-
-//         // Send the response to teams
-//         callback(null, response, 200);
-
-
-//         //}
-
-// };
-
-// connector.onQuery('Open', composeExtensionHandler);
-
-// var composeInvoke = function (event) {
-//     console.log(event);
-// };
-
-
-// connector.onInvoke('composeInvoke');
